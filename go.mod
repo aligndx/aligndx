9c509f7d4a52dba6bed6edf060a489194b83c577
mshunjan <manjothunjan@gmail.com>
2024-06-22 14:18:34 -0400

    Add default configuration for settings;

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/knadh/koanf/providers/env v0.1.0
 	github.com/knadh/koanf/v2 v2.1.1
+	github.com/nats-io/nats.go v1.36.0
 	github.com/pocketbase/dbx v1.10.1
 	github.com/pocketbase/pocketbase v0.22.14
 )
@@ -49,6 +50,7 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/klauspost/compress v1.17.2 // indirect
 	github.com/knadh/koanf/maps v0.1.1 // indirect
 	github.com/labstack/echo/v5 v5.0.0-20230722203903-ec5b858dab61 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -57,6 +59,8 @@ require (
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/nats-io/nkeys v0.4.7 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/spf13/cast v1.6.0 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -8,11 +8,15 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/knadh/koanf/providers/env"
 	"github.com/knadh/koanf/v2"
+	"github.com/nats-io/nats.go"
 )
 
 // Config struct holds configuration values
 type Config struct {
 	Nats NatsConfig `koanf:"nats"`
+	DB   DbConfig   `koanf:"db"`
+	SMTP SMTPConfig `koanf:"smtp"`
+	S3   S3Config   `koanf:"s3"`
 }
 
 // NatsConfig settings
@@ -21,10 +25,61 @@ type NatsConfig struct {
 	URL      string `koanf:"url"`
 }
 
+type DbConfig struct {
+	MigrationsDir string `koanf:"migrations_dir"`
+}
+
+type SMTPConfig struct {
+	Enabled  bool   `koanf:"smtp_enabled"`
+	Host     string `koanf:"smtp_host"`
+	Port     int    `koanf:"smtp_port"`
+	Password string `koanf:"smtp_password"`
+	Tls      bool   `koanf:"smtp_tls_enabled"`
+}
+
+type S3Config struct {
+	Enabled        bool   `koanf:"s3_enabled"`
+	Bucket         string `koanf:"s3_bucket"`
+	AccessKey      string `koanf:"aws_access_key"`
+	Secret         string `koanf:"aws_secret"`
+	Endpoint       string `koanf:"aws_endpoint"`
+	Region         string `koanf:"aws_region"`
+	ForcePathStyle bool   `koanf:"pathStyle"`
+}
+
 var (
 	cfg  *Config
 	once sync.Once
 )
+
+// NewConfig creates a new Config instance with default values
+func NewConfig() *Config {
+	return &Config{
+		Nats: NatsConfig{
+			InMemory: false,
+			URL:      nats.DefaultURL,
+		},
+		DB: DbConfig{
+			MigrationsDir: "migrations",
+		},
+		SMTP: SMTPConfig{
+			Enabled:  false,
+			Host:     "localhost",
+			Port:     25,
+			Password: "123456",
+			Tls:      false,
+		},
+		S3: S3Config{
+			Enabled:        false,
+			Bucket:         "default-bucket",
+			AccessKey:      "default-access-key",
+			Secret:         "default-secret",
+			Endpoint:       "default-endpoint",
+			Region:         "us-west-1",
+			ForcePathStyle: false,
+		},
+	}
+}
 
 // LoadConfig loads configuration from environment variables or a .env file
 func loadConfig() (*Config, error) {
