21fa2e40dea1b81f118863cd8b53793d7f5d980b
mshunjan <manjothunjan@gmail.com>
2024-07-30 17:37:36 -0400

    refactor(jobs): convert jobs into pluggable service layer

--- internal/jobs/jobs.go
+++ internal/jobs/jobs.go
@@ -2,13 +2,14 @@ package jobs
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
-	"sync"
 
 	"github.com/aligndx/aligndx/internal/config"
 	"github.com/aligndx/aligndx/internal/logger"
-	"github.com/nats-io/nats.go"
 )
+
+type JobHandler func(inputs map[string]interface{}) error
 
 type Job struct {
 	JobID     string                 `json:"job_id"`
@@ -17,50 +18,38 @@ type Job struct {
 	Status    string                 `json:"status"`
 }
 
-const (
-	streamName = "JOBS"
-)
-
-var (
-	nc   *nats.Conn
-	js   nats.JetStreamContext
-	once sync.Once
-	err  error
-	log  *logger.LoggerWrapper
-)
-
-func initJetStream() {
-	once.Do(func() {
-		configService := config.NewConfigService(log)
-		cfg := configService.LoadConfig()
-		nc, err = nats.Connect(cfg.Nats.URL)
-		if err != nil {
-			log.Fatal("Error connecting to NATS", map[string]interface{}{"error": err})
-		}
-
-		js, err = nc.JetStream()
-		if err != nil {
-			log.Fatal("Error creating JetStream context", map[string]interface{}{"error": err})
-		}
-
-		_, err = js.AddStream(&nats.StreamConfig{
-			Name:     streamName,
-			Subjects: []string{"jobs.*"},
-		})
-		if err != nil {
-			log.Fatal("Error creating stream", map[string]interface{}{"error": err})
-		}
-	})
+type JobServiceInterface interface {
+	QueueJob(jobID string, jobInputs map[string]interface{}, jobSchema string) error
+	GetJobInfo(jobID string) (*Job, error)
+	CancelJob(jobID string) error
+	RunJob(jobID string) error
+	RegisterJobHandler(schema string, handler JobHandler)
 }
 
-func getJetStream() nats.JetStreamContext {
-	initJetStream()
-	return js
+type MessageQueueService interface {
+	Publish(subject string, data []byte) error
+	GetLastMsg(subject string) ([]byte, error)
 }
 
-func queueJob(jobID string, jobInputs map[string]interface{}, jobSchema string) {
-	js := getJetStream()
+type JobService struct {
+	mq        MessageQueueService
+	log       *logger.LoggerWrapper
+	cfg       *config.Config
+	handlers  map[string]JobHandler
+	jobPrefix string
+}
 
+func NewJobService(mq MessageQueueService, log *logger.LoggerWrapper, cfg *config.Config, jobPrefix string) JobServiceInterface {
+	return &JobService{
+		mq:        mq,
+		log:       log,
+		cfg:       cfg,
+		handlers:  make(map[string]JobHandler),
+		jobPrefix: jobPrefix,
+	}
+}
+
+func (s *JobService) QueueJob(jobID string, jobInputs map[string]interface{}, jobSchema string) error {
 	job := Job{
 		JobID:     jobID,
 		JobInputs: jobInputs,
@@ -70,63 +59,98 @@ func queueJob(jobID string, jobInputs map[string]interface{}, jobSchema string) 
 
 	jobData, err := json.Marshal(job)
 	if err != nil {
-		log.Fatal("Error marshaling job data", map[string]interface{}{"error": err})
+		s.log.Error("Error marshaling job data", map[string]interface{}{"error": err})
+		return err
 	}
-
-	_, err = js.Publish(fmt.Sprintf("jobs.%s", jobID), jobData)
+	err = s.mq.Publish(fmt.Sprintf("%s.%s", s.jobPrefix, jobID), jobData)
 	if err != nil {
-		log.Fatal("Error publishing job", map[string]interface{}{"error": err})
+		s.log.Error("Error publishing job", map[string]interface{}{"error": err})
+		return err
 	}
 
-	log.Info("Job queued", map[string]interface{}{"job_id": jobID})
+	s.log.Info("Job queued", map[string]interface{}{"job_id": jobID})
+	return nil
 }
 
-func getJobInfo(jobID string) {
-	js := getJetStream()
+func (s *JobService) GetJobInfo(jobID string) (*Job, error) {
+	subject := fmt.Sprintf("%s.%s", s.jobPrefix, jobID)
 
-	subject := fmt.Sprintf("jobs.%s", jobID)
-
-	msg, err := js.GetLastMsg(streamName, subject)
+	msgData, err := s.mq.GetLastMsg(subject)
 	if err != nil {
-		log.Fatal("Error fetching job message", map[string]interface{}{"error": err})
+		s.log.Error("Error fetching job message", map[string]interface{}{"error": err})
+		return nil, err
 	}
 
 	var job Job
-	err = json.Unmarshal(msg.Data, &job)
+	err = json.Unmarshal(msgData, &job)
 	if err != nil {
-		log.Fatal("Error unmarshaling job data", map[string]interface{}{"error": err})
+		s.log.Error("Error unmarshaling job data", map[string]interface{}{"error": err})
+		return nil, err
 	}
 
-	log.Info("Job Info", map[string]interface{}{"job": job})
+	s.log.Info("Job Info", map[string]interface{}{"job": job})
+	return &job, nil
 }
 
-func cancelJob(jobID string) {
-	js := getJetStream()
-
-	subject := fmt.Sprintf("jobs.%s", jobID)
-
-	msg, err := js.GetLastMsg(streamName, subject)
+func (s *JobService) CancelJob(jobID string) error {
+	job, err := s.GetJobInfo(jobID)
 	if err != nil {
-		log.Fatal("Error fetching job message", map[string]interface{}{"error": err})
-	}
-
-	var job Job
-	err = json.Unmarshal(msg.Data, &job)
-	if err != nil {
-		log.Fatal("Error unmarshaling job data", map[string]interface{}{"error": err})
+		return err
 	}
 
 	job.Status = "cancelled"
 
 	jobData, err := json.Marshal(job)
 	if err != nil {
-		log.Fatal("Error marshaling job data", map[string]interface{}{"error": err})
+		s.log.Error("Error marshaling job data", map[string]interface{}{"error": err})
+		return err
 	}
-
-	_, err = js.Publish(subject, jobData)
+	err = s.mq.Publish(fmt.Sprintf("%s.%s", s.jobPrefix, jobID), jobData)
 	if err != nil {
-		log.Fatal("Error publishing job", map[string]interface{}{"error": err})
+		s.log.Error("Error publishing job", map[string]interface{}{"error": err})
+		return err
 	}
 
-	log.Info("Job cancelled", map[string]interface{}{"job_id": jobID})
+	s.log.Info("Job cancelled", map[string]interface{}{"job_id": jobID})
+	return nil
+}
+
+func (s *JobService) RunJob(jobID string) error {
+	job, err := s.GetJobInfo(jobID)
+	if err != nil {
+		return err
+	}
+
+	handler, exists := s.handlers[job.JobSchema]
+	if !exists {
+		s.log.Error("No handler registered for job schema", map[string]interface{}{"job_schema": job.JobSchema})
+		return errors.New("no handler registered for job schema")
+	}
+
+	err = handler(job.JobInputs)
+	if err != nil {
+		s.log.Error("Error running job", map[string]interface{}{"error": err, "job_id": jobID})
+		return err
+	}
+
+	job.Status = "completed"
+
+	jobData, err := json.Marshal(job)
+	if err != nil {
+		s.log.Error("Error marshaling job data", map[string]interface{}{"error": err})
+		return err
+	}
+	err = s.mq.Publish(fmt.Sprintf("%s.%s", s.jobPrefix, jobID), jobData)
+	if err != nil {
+		s.log.Error("Error publishing job", map[string]interface{}{"error": err})
+		return err
+	}
+
+	s.log.Info("Job completed", map[string]interface{}{"job_id": jobID})
+	return nil
+}
+
+func (s *JobService) RegisterJobHandler(schema string, handler JobHandler) {
+	s.handlers[schema] = handler
+	s.log.Info("Job handler registered", map[string]interface{}{"job_schema": schema})
 }
