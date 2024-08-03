1d05c89ee1093bc81211896714902d8c3f3c5fb9
mshunjan <manjothunjan@gmail.com>
2024-08-03 01:05:47 -0400

    style(ui): add new favicons

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -9,29 +9,6 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   description: siteConfig.description,
-  icons: [
-    {
-      rel: 'icon',
-      url: '/favicon/favicon.ico',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '16x16',
-      url: '/favicon/favicon-16x16.png',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '32x32',
-      url: '/favicon/favicon-32x32.png',
-    },
-    {
-      rel: 'apple-touch-icon',
-      sizes: '180x180',
-      url: '/favicon/apple-touch-icon.png',
-    },
-  ],
   manifest: `${siteConfig.url}/site.webmanifest`,
 }
 
