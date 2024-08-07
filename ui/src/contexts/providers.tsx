c0b06bb920e2667a8413908c3aff6ba5f5dbf3c2
mshunjan <manjothunjan@gmail.com>
2024-08-07 01:37:56 -0400

    feat(ui/contexts): add query context

--- ui/src/contexts/providers.tsx
+++ ui/src/contexts/providers.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import ThemeProvider from '@/components/theme-provider/theme-provider'
+import QueryProvider from "./query-context";
 
 interface ProvidersProps {
     children: ReactNode;
@@ -8,7 +9,9 @@ interface ProvidersProps {
 export default function Providers({ children }: ProvidersProps) {
     return (
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
-            {children}
+            <QueryProvider>
+                {children}
+            </QueryProvider>
         </ThemeProvider>
     )
 }
