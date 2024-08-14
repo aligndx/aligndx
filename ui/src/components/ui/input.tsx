cecb58293974b56c6ee8803367c4094b5f9d6c23
mshunjan <manjothunjan@gmail.com>
2024-08-14 03:31:14 -0400

    feat(ui/hooks): extract radial hover mouse effect to hook

--- ui/src/components/ui/input.tsx
+++ ui/src/components/ui/input.tsx
@@ -1,38 +1,21 @@
-import * as React from "react"
+import * as React from "react";
+import { motion } from "framer-motion";
+import { cn } from "@/lib/utils";
+import { useRadialHoverEffect } from "@/hooks/use-radial-hover-effect";
 
-import { cn } from "@/lib/utils"
-import { useMotionTemplate, useMotionValue, motion } from "framer-motion";
-
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> { }
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
-    const radius = 100; // change this to increase the rdaius of the hover effect
-    const [visible, setVisible] = React.useState(false);
-    let mouseX = useMotionValue(0);
-    let mouseY = useMotionValue(0);
+    const { background, handleMouseMove, handleMouseEnter, handleMouseLeave } =
+      useRadialHoverEffect(100);
 
-    function handleMouseMove({ currentTarget, clientX, clientY }: any) {
-      let { left, top } = currentTarget.getBoundingClientRect();
-
-      mouseX.set(clientX - left);
-      mouseY.set(clientY - top);
-    }
     return (
       <motion.div
-        style={{
-          background: useMotionTemplate`
-        radial-gradient(
-          ${visible ? radius + "px" : "0px"} circle at ${mouseX}px ${mouseY}px,
-          var(--blue-500),
-          transparent 80%
-        )
-      `,
-        }}
+        style={{ background }}
         onMouseMove={handleMouseMove}
-        onMouseEnter={() => setVisible(true)}
-        onMouseLeave={() => setVisible(false)}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
         className="p-[2px] rounded-lg transition duration-300 group/input"
       >
         <input
@@ -45,9 +28,10 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           {...props}
         />
       </motion.div>
-    )
+    );
   }
-)
-Input.displayName = "Input"
+);
 
-export { Input }
+Input.displayName = "Input";
+
+export { Input };
