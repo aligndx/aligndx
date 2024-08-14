29ab92f57882bbab64dc44456e39af0eef75b6d9
mshunjan <manjothunjan@gmail.com>
2024-08-14 19:34:24 -0400

    fix(layouts/mobile-nav): patch mobile nav missing description and title with header

--- ui/src/layouts/common/mobile-nav.tsx
+++ ui/src/layouts/common/mobile-nav.tsx
@@ -4,6 +4,7 @@ import Logo from "@/components/logo";
 import {
     Sheet,
     SheetContent,
+    SheetDescription,
     SheetHeader,
     SheetTitle,
 } from "@/components/ui/sheet"
@@ -20,6 +21,10 @@ interface MobileSideNavProps {
 export function MobileSideNav({ children, showSideNav, toggleSideNav }: MobileSideNavProps) {
     return (
         <Sheet open={showSideNav} onOpenChange={toggleSideNav}>
+            <SheetHeader>
+                <SheetTitle></SheetTitle>
+                <SheetDescription></SheetDescription>
+            </SheetHeader>
             <SheetContent side="left">
                 {children}
             </SheetContent>
