dfd80152a0eb77bb605987d1f6b3973bd7325c7a
mshunjan <manjothunjan@gmail.com>
2024-08-20 23:38:32 -0400

    feat(services/api): update api service hook with data service

--- ui/src/services/api/api-service.ts
+++ ui/src/services/api/api-service.ts
@@ -3,18 +3,21 @@ import useUserService from './user-service';
 import useWorkflowService from './workflow-service';
 import { client } from './client';
 import useSubmissionService from './submission-services';
+import useDataService from './data-service';
 
 const useApiService = () => {
   const authService = useAuthService(client);
   const userService = useUserService(client);
   const submissionService = useSubmissionService(client);
   const workflowService = useWorkflowService(client);
+  const dataService = useDataService(client);
 
   return {
     auth: authService,
     user: userService,
+    submissions: submissionService,
     workflows: workflowService,
-    submissions: submissionService
+    data: dataService,
   };
 };
 
