d31bea59a38cc14d22c4209257f2f8dd160247f4
mshunjan <manjothunjan@gmail.com>
2024-09-02 17:05:14 -0400

    feat(data/file-grid.tsx): add folder + file sections and create a DataCard

--- ui/src/sections/data/file-grid.tsx
+++ ui/src/sections/data/file-grid.tsx
@@ -10,27 +10,29 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { Button } from "@/components/ui/button";
 import { Data } from "@/types/data";
 import createHumanReadableDate from "@/lib/date";
-import { Input } from "@/components/ui/input";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 type FileGridProps = {
     data: Data[];
     isLoading: boolean;
-    onDownload: (file : Data) => void;
+    onDownload: (file: Data) => void;
     onRename: (id: string, newName: string) => void;
 };
 
 const FileGrid: React.FC<FileGridProps> = ({ data, isLoading, onDownload, onRename }) => {
     const [currentPath, setCurrentPath] = useState<string | null>(''); // Track the current folder path
-    const [currentFiles, setCurrentFiles] = useState<Data[]>([]);
+    const [currentData, setCurrentData] = useState<Data[]>([]);
 
     // Update the current files to display based on the current path
     useEffect(() => {
         if (!isLoading) {
             const filteredFiles = data.filter((item) => item.parent === currentPath);
-            setCurrentFiles(filteredFiles);
+            setCurrentData(filteredFiles);
         }
     }, [currentPath, data, isLoading]);
+
+    const folders = currentData.filter((file: Data) => file.type === 'folder');
+    const files = currentData.filter((file: Data) => file.type === 'file');
 
     const handleFolderClick = (folderId: string | undefined) => {
         if (folderId) {
@@ -69,77 +71,110 @@ const FileGrid: React.FC<FileGridProps> = ({ data, isLoading, onDownload, onRena
                         Back
                     </Button>
                 )}
-            </div>
-            <div className="flex items-center justify-between">
-                <Input
-                    placeholder="Search your data..."
-                    className="max-w-sm"
-                />
-            </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {currentFiles.map((file) => (
-                    <Card
-                        key={file.id}
-                        className="hover:bg-muted hover:scale-[1.03] cursor-pointer"
-                        onClick={() => file.type === "folder" && handleFolderClick(file.id)}
-                    >
-                        <CardHeader>
-                            <CardTitle>
-                                <div className="flex flex-col gap-2">
-                                    <div className="flex flex-row gap-2 justify-between ">
-                                        <div className="flex gap-2 items-center text-sm overflow-hidden">
-                                            <div>
-                                                {file.type === "folder" ? <FolderIcon /> : <FileIcon />}
-                                            </div>
-                                            <div className="overflow-hidden text-ellipsis whitespace-nowrap min-w-0">
-                                                {file.name}
-                                            </div>
-                                        </div>
-                                        <div>
-                                            <DropdownMenu key={file.id}>
-                                                <DropdownMenuTrigger asChild>
-                                                    <Button variant="icon" size="icon">
-                                                        <MoreVerticalIcon className="h-4 w-4" />
-                                                        <span className="sr-only">More</span>
-                                                    </Button>
-                                                </DropdownMenuTrigger>
-                                                <DropdownMenuContent>
-                                                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                                                    <DropdownMenuSeparator />
-                                                    <DropdownMenuItem onClick={() => onDownload(file)}>
-                                                        <div className="flex flex-row items-center gap-4 text-sm">
-                                                            <div>
-                                                                <DownloadIcon className="h-4 w-4" />
-                                                            </div>
-                                                            <div>
-                                                                Download
-                                                            </div>
-                                                        </div>
-                                                    </DropdownMenuItem>
-                                                    <DropdownMenuItem>
-                                                        <div className="flex flex-row items-center gap-4 text-sm">
-                                                            <EditIcon className="h-4 w-4" />
-                                                            Rename
-                                                        </div>
-                                                    </DropdownMenuItem>
-                                                </DropdownMenuContent>
-                                            </DropdownMenu>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </CardTitle>
-                        </CardHeader>
-                        <CardFooter>
-                            <div className="text-muted-foreground text-xs">
-                                {file.updated && createHumanReadableDate(file.updated)}
+            </div> 
+            <div>
+                <div className="flex flex-col gap-4">
+                    {folders.length > 0 && (
+                        <div className="flex flex-col gap-4">
+                            <h3>Folders</h3>
+                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                                {folders.map((file) => (
+                                    <DataCard
+                                        key={file.id}
+                                        file={file}
+                                        handleFolderClick={handleFolderClick}
+                                        onDownload={onDownload}
+                                    />
+                                ))}
                             </div>
-                        </CardFooter>
-                    </Card>
-                ))}
+                        </div>
+                    )}
+                    {files.length > 0 && (
+                        <div className="flex flex-col gap-4">
+                            <h3>Files</h3>
+                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                                {files.map((file) => (
+                                    <DataCard
+                                        key={file.id}
+                                        file={file}
+                                        handleFolderClick={handleFolderClick}
+                                        onDownload={onDownload}
+                                    />
+                                ))}
+                            </div>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
 };
+
+interface DataCardProps {
+    file: Data;
+    handleFolderClick: (id: string) => void;
+    onDownload: (file: Data) => void;
+}
+
+const DataCard = ({ file, handleFolderClick, onDownload }: DataCardProps) => {
+    return (
+        <Card
+            className="hover:bg-muted hover:scale-[1.03] cursor-pointer"
+            onClick={() => file.type === "folder" && handleFolderClick(file.id)}
+        >
+            <CardHeader>
+                <CardTitle>
+                    <div className="flex flex-col gap-2">
+                        <div className="flex flex-row gap-2 justify-between ">
+                            <div className="flex gap-2 items-center text-sm overflow-hidden">
+                                <div>
+                                    {file.type === "folder" ? <FolderIcon /> : <FileIcon />}
+                                </div>
+                                <div className="overflow-hidden text-ellipsis whitespace-nowrap min-w-0">
+                                    {file.name}
+                                </div>
+                            </div>
+                            <div>
+                                <DropdownMenu key={file.id}>
+                                    <DropdownMenuTrigger asChild>
+                                        <Button variant="icon" size="icon">
+                                            <MoreVerticalIcon className="h-4 w-4" />
+                                            <span className="sr-only">More</span>
+                                        </Button>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent>
+                                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                                        <DropdownMenuSeparator />
+                                        <DropdownMenuItem onClick={() => onDownload(file)}>
+                                            <div className="flex flex-row items-center gap-4 text-sm">
+                                                <div>
+                                                    <DownloadIcon className="h-4 w-4" />
+                                                </div>
+                                                <div>
+                                                    Download
+                                                </div>
+                                            </div>
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem>
+                                            <div className="flex flex-row items-center gap-4 text-sm">
+                                                <EditIcon className="h-4 w-4" />
+                                                Rename
+                                            </div>
+                                        </DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
+                            </div>
+                        </div>
+                    </div>
+                </CardTitle>
+            </CardHeader>
+            <CardFooter>
+                <div className="text-muted-foreground text-xs">
+                    {file.updated && createHumanReadableDate(file.updated)}
+                </div>
+            </CardFooter>
+        </Card>
+    )
+}
 
 export default FileGrid;
