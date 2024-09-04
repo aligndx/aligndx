c721c01430a5e30eef921ab73dcc22fa97998cd4
mshunjan <manjothunjan@gmail.com>
2024-09-04 16:04:33 -0400

    feat(ui): add resizable component

--- ui/src/sections/insights/insights-view.tsx
+++ ui/src/sections/insights/insights-view.tsx
@@ -1,9 +1,17 @@
 'use client'
 
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+  } from "@/components/ui/resizable"
+  
+
 export default function InsightsView() {
     return (
-        <>
-            test
-        </>
+        <div className="grid grid-cols-3 gap-2 flex-grow h-full">
+            <div className="col-span-2">test</div>
+            <div className="bg-muted">test</div>
+        </div>
     )
 }
