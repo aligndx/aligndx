d02f1a81e5b2dbb2f664317266d8022e4807e092
mshunjan <manjothunjan@gmail.com>
2024-09-11 09:01:17 -0400

    refactor(workflow.tsx): simplify workflow page

--- ui/src/sections/workflows/workflow/workflow.tsx
+++ ui/src/sections/workflows/workflow/workflow.tsx
@@ -18,34 +18,12 @@ export default function Workflow() {
             <header className="px-6 py-2 sticky text-2xl" >
                 {workflow.data?.name || <Skeleton className="w-32 h-6" />}
             </header>
-            <Tabs defaultValue="parameters" className="w-full flex flex-col flex-grow">
-                <TabsList
-                    variant="underlined"
-                    className="w-full justify-start"
-                >
-                    <TabsTrigger
-                        value="parameters"
-                    >
-                        Parameters
-                    </TabsTrigger>
-                    <TabsTrigger
-                        value="runs"
-                    >
-                        Runs
-                    </TabsTrigger>
-                </TabsList>
-                <TabsContent value="parameters" className="flex flex-grow">
-                    {
-                        workflow.isLoading ?
-                            <WorkflowFormSkeleton />
-                            :
-                            <WorkflowForm id={workflow.data?.id || ""} name={workflow.data?.name || ""} repository={workflow.data?.repository || ""} description={workflow.data?.description || ""} jsonSchema={workflow.data?.schema} />
-                    }
-                </TabsContent>
-                <TabsContent value="runs" className="p-4">
-                    Example runs
-                </TabsContent>
-            </Tabs>
+            {
+                workflow.isLoading ?
+                    <WorkflowFormSkeleton />
+                    :
+                    <WorkflowForm id={workflow.data?.id || ""} name={workflow.data?.name || ""} repository={workflow.data?.repository || ""} description={workflow.data?.description || ""} jsonSchema={workflow.data?.schema} />
+            }
         </div>
     )
 }
