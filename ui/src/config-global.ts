4d93354bc4f964289fb91f3195b72a308ff3b0a6
mshunjan <manjothunjan@gmail.com>
2024-11-14 12:06:37 -0500

    refactor(config-global.ts): provide fallback for environment variables

--- ui/src/config-global.ts
+++ ui/src/config-global.ts
@@ -1,4 +1,4 @@
-export const API = process.env.API_URL;
-export const ENVIRONMENT = process.env.NODE_ENV;
-export const APDB = process.env.APDB_URL;
-export const APDB_RAW = process.env.APDB_RAW_URL
+export const API = process.env.API_URL || "";
+export const ENVIRONMENT = process.env.NODE_ENV || "";
+export const APDB = process.env.APDB_URL || "";
+export const APDB_RAW = process.env.APDB_RAW_URL || "";
