824a076c1de51d9f74e495efe556c3ad941dbc9f
mshunjan <manjothunjan@gmail.com>
2024-11-20 14:42:07 -0500

    fix(deploy): patch ui + admin in deployment

--- ui/src/config-global.ts
+++ ui/src/config-global.ts
@@ -1,4 +1,4 @@
-export const API = process.env.API_URL || "";
-export const ENVIRONMENT = process.env.NODE_ENV || "";
-export const APDB = process.env.APDB_URL || "";
-export const APDB_RAW = process.env.APDB_RAW_URL || "";
+export const API = process.env.NEXT_PUBLIC_API_URL as string;
+export const ENVIRONMENT = process.env.NODE_ENV;
+export const APDB = process.env.NEXT_PUBLIC_APDB_URL as string;
+export const APDB_RAW = process.env.NEXT_PUBLIC_APDB_RAW_URL as string;
