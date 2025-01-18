1953af720893d2bf37ac415cbec61931c6661d3d
mshunjan <manjothunjan@gmail.com>
2025-01-18 16:38:45 -0500

    feat(ui): delete data page + overview section

--- ui/src/app/dashboard/data/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FileManagerView } from "@/sections/data";
-
-export const metadata = {
-    title: 'Data',
-};
-
-export default function Page() {
-    return <FileManagerView />
-}
--- ui/src/sections/overview/view/overview.tsx
+++
@@ -1,7 +0,0 @@
-export default function Overview() {
-    return (
-        <div className="p-6">
-            Dashboard Overview
-        </div>
-    )
-}
