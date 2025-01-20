7c2ba8305939c73fc4ee67abb6bc2393fa7cd307
mshunjan <manjothunjan@gmail.com>
2025-01-20 16:29:09 -0500

    feat(submission-selector.tsx): add loading skeleton

--- ui/src/sections/explore/insights/submission-selector.tsx
+++ ui/src/sections/explore/insights/submission-selector.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import { Combobox } from "@/components/ui/combobox"; // Adjust the import path as needed
 import { useApiService } from "@/services/api";
 import { Submission, Status } from "@/types/submission";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface SubmissionSelectorProps {
   value?: Submission[];
@@ -29,7 +30,7 @@ export function SubmissionSelector({
 
   // While loading, you might render a placeholder or spinner
   if (isLoading) {
-    return <div>Loading submissions...</div>;
+    return <SubmissionSelectorSkeleton />
   }
 
   return (
@@ -40,10 +41,25 @@ export function SubmissionSelector({
       multiple={multiple}
       disabled={disabled}
       label="Source(s)"
-      title="Select submission(s)"
+      title="Submission(s)"
       description="Select the submissions you want to analyze"
       searchPlaceholder="Search submissions..."
       itemToString={(item) => item.name}
     />
   );
+}
+
+function SubmissionSelectorSkeleton() {
+  return (
+      <div className="flex flex-col space-y-3">
+          <div className="space-y-2">
+              <Skeleton className="h-4 w-[70px] rounded-xl" />
+              <Skeleton className="h-4 w-[250px]" />
+          </div>
+          <div className="flex gap-1">
+              <Skeleton className="h-10 w-[200px]" />
+          </div>
+      </div>
+
+  )
 }
