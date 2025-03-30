dbaea4c1a6b7c959bbd12e7f3e6d44f473676be6
mshunjan <manjothunjan@gmail.com>
2025-03-30 19:35:38 -0400

    feat: add archive type to data collection

---
+++ internal/migrations/1743377272_updated_data.go
@@ -0,0 +1,61 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("270xb773aehpc4p")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(2, []byte(`{
+			"hidden": false,
+			"id": "m2wyxqmq",
+			"maxSelect": 1,
+			"name": "type",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "select",
+			"values": [
+				"file",
+				"folder",
+				"archive"
+			]
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("270xb773aehpc4p")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(2, []byte(`{
+			"hidden": false,
+			"id": "m2wyxqmq",
+			"maxSelect": 1,
+			"name": "type",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "select",
+			"values": [
+				"file",
+				"folder"
+			]
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
